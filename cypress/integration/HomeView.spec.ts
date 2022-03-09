bed6df54c96c2bce8e8f499ddba575a20c85b3d1
Andrey Zolotarev <andrey.zolotarev@umbrellait.com>
2022-03-09 12:59:47 +0300

    Add UnotTests and Cypress e2e test into GitLab CI

---
+++ cypress/integration/HomeView.spec.ts
@@ -0,0 +1,8 @@
+// https://docs.cypress.io/api/introduction/api.html
+
+describe("/", () => {
+	it("visits the app home url", () => {
+		cy.visit("/")
+		cy.contains("div", "PawaTask")
+	})
+})
--- src/components/__tests__/HomeView.spec.ts
+++ src/components/__tests__/HomeView.spec.ts
@@ -1,11 +1,15 @@
 import { describe, it, expect } from "vitest"
-
 import { mount } from "@vue/test-utils"
-import HomeView from "src/views/TaskListView.vue"
+import { store } from "@/store"
+import HomeView from "@/views/TaskListView.vue"
 
 describe("HomeView", () => {
 	it("renders properly", () => {
-		const wrapper = mount(HomeView)
-		expect(wrapper.text()).toContain("Task list")
+		const wrapper = mount(HomeView, {
+			global: {
+				plugins: [store],
+			},
+		})
+		expect(wrapper.text()).toContain("Add a new task")
 	})
 })
